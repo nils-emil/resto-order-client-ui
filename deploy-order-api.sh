71e9ab7ca744701d7bd048c329b2681e21e54988
nilsemil <nilsemil.lille@gmail.com>
2020-05-05 01:41:26 +0300

    production env variables

--- deploy-order-api.sh
+++ deploy-order-api.sh
@@ -1,7 +1,10 @@
 ssh -i .\aws-resto.pem ubuntu@ec2-13-49-78-61.eu-north-1.compute.amazonaws.com
 pm2 stop app
 cd /home/ubuntu/chatRestoraunt/order-api || exit
+git reset --hard
 git pull
+rm config/db.js
+cp config/db-prod.js config/db.js
 npm i
 pm2 start app.js
 exit
