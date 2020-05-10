d270bd536ab203e4510f68e0a315e3ef00257c86
nilsemil <nilsemil.lille@gmail.com>
2020-05-10 14:24:04 +0300

    deployment scripts

--- deploy-client-ui.sh
+++ deploy-client-ui.sh
@@ -1,2 +1,2 @@
-scp  -i .\aws-resto.pem -r .\client-ui\build\* ubuntu@ec2-13-49-78-61.eu-north-1.compute.amazonaws.com:/var/www/html
+scp  -i aws-resto.pem -r client-ui/build/* ubuntu@ec2-13-49-78-61.eu-north-1.compute.amazonaws.com:/var/www/html
 exit
--- deploy-order-api.sh
+++ deploy-order-api.sh
@@ -1,4 +1,5 @@
-ssh -i .\aws-resto.pem ubuntu@ec2-13-49-78-61.eu-north-1.compute.amazonaws.com
+ssh -i aws-resto.pem ubuntu@ec2-13-49-78-61.eu-north-1.compute.amazonaws.com << EOF
+set -o xtrace
 pm2 stop app
 cd /home/ubuntu/chatRestoraunt/order-api || exit
 git reset --hard
@@ -7,4 +8,4 @@ rm config/db.js
 cp config/db-prod.js config/db.js
 npm i
 pm2 start app.js
-exit
+EOF
