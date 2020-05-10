128a0850067722c0b3e08ffc441b6e2644ac2c8b
KarlMae <karl@mae.ee>
2020-05-10 18:47:42 +0300

    Add error handling for file upload

--- admin-ui/src/components/Modal/Types/ImageUploadModal/ImageUploadModal.js
+++ admin-ui/src/components/Modal/Types/ImageUploadModal/ImageUploadModal.js
@@ -4,6 +4,8 @@ import Modal from '../../Modal'
 import './styles.scss'
 import { connect } from 'react-redux'
 import { uploadImage } from '../../../../services/imageService'
+import { showPopUpWithTimeout } from '../../../../store/actions/popup'
+import { popUpVariants } from '../../../PopUp/PopUp'
 
 function ImageUploadModal(props) {
 
@@ -21,24 +23,29 @@ function ImageUploadModal(props) {
     props.hideModal()
   }
 
-  const dialogStyle = {
-  }
-
   const onChange = (e) => {
-    const files = Array.from(e.target.files)
+    if (e.target.files[0].size > 5000000) {
+      props.showPopUpWithTimeout({ type: popUpVariants.ERROR, text: 'Fail on liiga suur' })
+      return
+    }
+
     setLoadingState(true)
-
     const formData = new FormData()
-    files.forEach((file, i) => {
-      formData.append('image', file)
-    })
+    formData.append('image', e.target.files[0])
 
-    uploadImage(formData).subscribe(response => {
+    const observer = {
+      next: response => {
         setImageName(response.data.imageName)
         setImageUrl(response.data.imageUrl)
         setLoadingState(false)
+      },
+      error: error => {
+        props.showPopUpWithTimeout({ type: popUpVariants.ERROR, text: error.response.data.message })
+        setLoadingState(false)
       }
-    )
+    }
+
+    uploadImage(formData).subscribe(observer)
   }
 
   return (
@@ -47,7 +54,7 @@ function ImageUploadModal(props) {
       onClose={onClose}
       onConfirm={onConfirm}
       confirmButtonText={'Nõustu'}
-      dialogStyle={dialogStyle}
+      dialogStyle={{}}
       isLoading={isLoading}
     >
       <div>
@@ -63,7 +70,8 @@ function ImageUploadModal(props) {
 }
 
 const mapDispatchToProps = dispatch => ({
-  hideModal: () => dispatch(hideModal())
+  hideModal: () => dispatch(hideModal()),
+  showPopUpWithTimeout: (popUpType, popUpText) => dispatch(showPopUpWithTimeout(popUpType, popUpText))
 })
 
 export default connect(null, mapDispatchToProps)(ImageUploadModal)
--- order-api/api/protected/imageController.js
+++ order-api/api/protected/imageController.js
@@ -6,12 +6,17 @@ const upload = require('../../services/imageService')
 const singleUpload = upload.single('image')
 
 router.post('/upload', function (req, res) {
-
   singleUpload(req, res, function (err) {
-    return res.json({
-      'imageUrl': req.file.location,
-      'imageName': req.file.originalname
-    })
+    if (!err) {
+      return res.json({
+        'imageUrl': req.file.location,
+        'imageName': req.file.originalname
+      })
+    } else {
+      return res.status(400).json({
+        message: 'File upload failed'
+      })
+    }
   })
 })
 
