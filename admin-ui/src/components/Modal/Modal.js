1e92ac2a9e0dc8aaf8fb64b48f97d729f2495443
KarlMae <karl@mae.ee>
2020-05-10 13:22:01 +0300

    Image upload

--- admin-ui/src/components/Modal/Modal.js
+++ admin-ui/src/components/Modal/Modal.js
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react'
 import './styles.scss'
 import TextButton, { buttonModifiers } from '../TextButton/TextButton'
+import { YinYang } from '../../resources/icons_index'
 
 function Modal(props) {
-  const { onClose, onConfirm, children, header, dialogStyle, confirmButtonText = 'Salvesta' } = props
+  const { onClose, onConfirm, children, header, dialogStyle, confirmButtonText = 'Salvesta', isLoading = false } = props
 
   useEffect(() => {
     window.addEventListener('keydown', listenKeyboard, true)
@@ -29,17 +30,31 @@ function Modal(props) {
     event.stopPropagation()
   }
 
+  const content = () => {
+    if (isLoading) {
+      return <YinYang className="modal__loading-spinner"/>
+    } else {
+      return (
+        <>
+          <h1 className="modal__header">{header}</h1>
+          <div className="modal__content">
+            {children}
+          </div>
+          <div className="modal__footer">
+            <TextButton onClick={onClose}
+                        modifiers={[buttonModifiers.TRANSPARENT, buttonModifiers.SMALL, buttonModifiers.INLINE]}>Tühista</TextButton>
+            <TextButton onClick={onConfirm}
+                        modifiers={[buttonModifiers.SMALL, buttonModifiers.INLINE]}>{confirmButtonText}</TextButton>
+          </div>
+        </>
+      )
+    }
+  }
+
   return (
     <div className="modal" onClick={onOverlayClick}>
       <div className="modal__dialog" style={dialogStyle} onClick={onDialogClick}>
-        <h1 className="modal__header">{header}</h1>
-        <div className="modal__content">
-          {children}
-        </div>
-        <div className="modal__footer">
-          <TextButton onClick={onClose} modifiers={[buttonModifiers.TRANSPARENT, buttonModifiers.SMALL, buttonModifiers.INLINE]}>Tühista</TextButton>
-          <TextButton onClick={onConfirm} modifiers={[buttonModifiers.SMALL, buttonModifiers.INLINE]}>{confirmButtonText}</TextButton>
-        </div>
+        {content()}
       </div>
     </div>
   )
